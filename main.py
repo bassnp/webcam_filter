3393c9ff24b6d1a6eb79dbed9ac9305f36bd74be
not.bass <34872213+bassnp@users.noreply.github.com>
2025-02-04 18:39:51 -0800

    - redid frame modification pipeline - added new filter

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import datetime
 from math import floor
-
 import cv2
 import torch
 import math
 import pyvirtualcam
 from pyvirtualcam import PixelFormat
-import matplotlib.pyplot as plt
 import numpy as np
 
 def int_to_clr(n):
@@ -15,27 +12,29 @@ def int_to_clr(n):
     b = int((math.tan(n) * 1000) % 256)
     return r, g, b
 
-def draw_box(frame, box, txt, r, g, b):
-    x, y, x2, y2, conf, cls = box
-    x, y = int(x), int(y)
-    w, h = int(x2-x), int(y2-y)
-
-    clr = (r, g, b)
-    conf_clr = (int(r * conf), int(g * conf), int(b * conf))
-    conf_perc = floor(conf * 10000) / 100
-    conf_brightness = int(conf * 256)
-    conf_blackwhite = (conf_brightness, conf_brightness, conf_brightness)
-
+def blend_rect(frame, x, y, w, h, clr):
     sub_img = frame[y:y+h, x:x+w]
     shader_img  = np.full(sub_img.shape, clr, np.uint8)
     blend = cv2.addWeighted(sub_img, 0.35, shader_img, 0.35, 1.0)
     frame[y:y+h, x:x+w] = blend
 
-    cv2.rectangle(frame, (int(x), int(y)), (int(x2), int(y2)), clr, 2)
-    cv2.putText(frame, txt, (int(x + 5), int(y - 10)), cv2.FONT_HERSHEY_DUPLEX, 0.8, clr, 1)
+def draw_ai_label(frame, txt, x, y, clr, conf):
+    conf_perc = floor(conf * 10000) / 100
+    conf_brightness = int(conf * 256)
+    conf_blackwhite = (conf_brightness, conf_brightness, conf_brightness)
+    cv2.putText(frame, txt, (int(x + 5), int(y - 10)), cv2.FONT_HERSHEY_DUPLEX, 0.8, clr if clr != -1 else conf_blackwhite, 1)
     cv2.putText(frame, str(conf_perc), (int(x + 5), int(y + 15)), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, conf_blackwhite, 1)
 
-def scan_frame(tlist, frame, model):
+def draw_ai_box(frame, box, txt, r, g, b):
+    x, y, x2, y2, conf, cls = box
+    x, y = int(x), int(y)
+    w, h = int(x2-x), int(y2-y)
+    clr = (r, g, b)
+
+    blend_rect(frame, x, y, w, h, clr)
+    draw_ai_label(frame, txt, x, y, clr, conf)
+
+def ai_filter(frame, model):
     results = model(frame)
     boxes = results.xyxy[0].cpu().numpy()
     sightings = []
@@ -49,41 +48,74 @@ def scan_frame(tlist, frame, model):
         if name not in sightings: # Quick log
             sightings.append(name)
 
-        draw_box(frame, box, name, r, g, b)
+        # draw_ai_box(frame, box, name, r, g, b)
+        draw_ai_box(frame, box, name, r, g, b)
 
     return frame, sightings
 
-def main(track_list = [], capture_list = []):
+def hue_filter(frame, model):
+    results = model(frame)
+    boxes = results.xyxy[0].cpu().numpy()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    image_gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    dark_img  = np.full(image_gray.shape, (0, 0, 0), np.uint8)
+    blend = cv2.addWeighted(image_gray, 0.4, dark_img, 0.4, 1.0)
+
+    for box in boxes:
+        x, y, x2, y2, conf, cls = box
+        x, y = int(x), int(y)
+        w, h = int(x2-x), int(y2-y)
+        blend[y:y+h, x:x+w] = frame[y:y+h, x:x+w]
+
+    for box in boxes:
+        x, y, x2, y2, conf, cls = box
+        x, y = int(x), int(y)
+        w, h = int(x2-x), int(y2-y)
+        name = model.names[int(cls)]
+        name = 'idiot' if name == 'person' else name
+        draw_ai_label(blend, name, x, y, -1, conf)
+
+
+    return blend
+
+def main(filter_name):
     print(torch.__version__)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using " + str(device))
 
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    # Image scanning model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5x')
     model.to(device)
 
+    # Webcam capture
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         exit()
 
-    last_sighting = None
-    print("Setupcam " + str(device))
-    with pyvirtualcam.Camera(width=640, height=480, fps=60, fmt=PixelFormat.BGR, backend ='unitycapture') as cam:
+    # Virtual Camera
+    with (pyvirtualcam.Camera(width=640, height=480, fps=60, fmt=PixelFormat.BGR, backend ='unitycapture') as cam):
+        # Webcam Loop
         while cap.isOpened():
-            ret, frame = cap.read()
+            ret, frame = cap.read() # Read webcam frame/image
             if ret is not None and frame is not None:
-                # Scan Image
-                named_frame, sightings = scan_frame(track_list, frame, model)
+                # Switch desired filter name
+                frame_filtered = None
+                if filter_name == "ai":
+                    frame_filtered, sightings = ai_filter(frame, model)
+                elif filter_name == "hue":
+                    frame_filtered = hue_filter(frame, model)
+                else: # Default
+                    frame_filtered = frame
 
-                # Show window
-                half_size_frame = cv2.resize(named_frame, (0, 0), fx=0.3, fy=0.3)
+                # Show window debug
+                half_size_frame = cv2.resize(frame_filtered, (0, 0), fx=0.3, fy=0.3)
                 cv2.imshow("webcam", half_size_frame)
                 cv2.setWindowProperty("webcam", cv2.WND_PROP_TOPMOST, 1)
 
-                cv2.moveWindow("webcam", 50, 35)
-
                 # Send to virtual cam
-                cam.send(named_frame)
+                cam.send(frame_filtered)
 
             if cv2.waitKey(1) == ord('q'): # Quit on q
                 break
@@ -93,10 +125,4 @@ def main(track_list = [], capture_list = []):
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main(
-        # Specific tracking
-        [],
-
-        # Specific capturing
-        ["bird"]
-    )
+    main("hue")
